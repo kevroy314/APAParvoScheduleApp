611559245912dccca8e9a6a9b6ad353ddf2932f1
Kevin H <kevin.horecka@gmail.com>
2012-09-12 18:57:33 -0500

    Initial Commit
    Some basic functionality including a heatmap grid which takes a 1D array
    of ints, binded clicking between the calendar and heat map, and some
    simple layout stuff.
    
    Working on tooltips now then, ultimately, integration with a database
    containing monthly schedule data.
    
    This is just the client side scheduler. An additional administrative
    page will need to be added as well as a login so users can determine
    their schedules.

---
+++ APAParvoScheduleClient/APAParvoScheduleClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("APAParvoScheduleClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APAParvoScheduleClient")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b658d724-cfdf-454d-bcc5-0c0b4db2c0ef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
